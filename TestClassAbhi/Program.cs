8c89c7058d5a40d2567379316fe11773d71451de
asingh1185 <asingh1185@W1A18-A05>
2023-02-21 20:11:05 -0500

    Add project files.

---
+++ TestClassAbhi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassAbhi
+{
+    //Abhijit Singh
+    public static class Triangle
+    {
+        static void Main(String[] args) { }
+        public static string ValidTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 90)
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+    }
+}
